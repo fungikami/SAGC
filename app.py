744dc3d4374ea3fa0c69118b10c6aa4a5d16c751
fungikami <kafung09@gmail.com>
2022-06-04 09:35:54 -0400

    Base de la Database

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/users.db'
+
+# Crear modelo de usuario  
+# class User(db.Model, UserMixin):
+#     __tablename__ = 'user'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), nullable=False, unique=True)
+#     email = db.Column(db.String(200))
+#    password = db.Column(db.String(80), nullable=False) 
+
+# Crear formularios de login y register
+
+
 
 @app.route("/")
 def index():
