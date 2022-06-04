4b98e63f75ad8f6eaae3ee25bd94c328ee0a3d20
fungikami <kafung09@gmail.com>
2022-06-04 14:19:13 -0400

    Estructura del login

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/users.db'
+# db = SQLAlchemy(app)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/users.db'
+app.config['SECRET_KEY'] = 'unaclavesecreta'
 
 # Crear modelo de usuario  
 # class User(db.Model, UserMixin):
@@ -13,17 +16,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/users.db'
 #     email = db.Column(db.String(200))
 #    password = db.Column(db.String(80), nullable=False) 
 
-# Crear formularios de login y register
-
-
+# Decorador de login requerido (para hacer logout hay que estar login)
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Necesitas iniciar sesión primero.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route("/")
-def index():
+@login_required
+def home():
     return render_template("home.html")
 
-@app.route("/login")
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None 
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Credenciales inválidas. Intentalo de nuevo.'
+        else:
+            session['logged_in'] = True
+            flash('Te has conectado.')
+            return redirect(url_for('home'))
+    return render_template("login.html", error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('Te has desconectado.')
+    return redirect(url_for('welcome'))
 
 @app.route("/register")
 def register():
