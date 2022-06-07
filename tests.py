876c86ca3898087ae9391e09fba7125cbeb46a24
fungikami <kafung09@gmail.com>
2022-06-06 22:05:04 -0400

    Tests

--- tests.py
+++ tests.py
@@ -26,13 +26,16 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/login')
         self.assertIn(b'Iniciar', response.data)
 
-    # Verifica que /portafolio y /logout requieren de haber iniciado sesión
+    # Verifica que /portafolio /perfiles /eventos y /logout requieren de haber iniciado sesión
     def test_route_requires_login(self):
         tester = app.test_client()
         response = tester.get('/perfiles', follow_redirects=True)
         self.assertIn(b'Necesitas iniciar ', response.data)
 
         response = tester.get('/portafolio', follow_redirects=True)
+        self.assertIn(b'Necesitas iniciar ', response.data)
+
+        response = tester.get('/eventos', follow_redirects=True)
         self.assertIn(b'Necesitas iniciar ', response.data)
 
         response = tester.get('/logout', follow_redirects=True)
@@ -45,22 +48,26 @@ class FlaskTestCase(unittest.TestCase):
     # Verifica que el login funciona correctamente cuando se dan las credenciales correctas
     def test_correct_login(self):
         tester = app.test_client()
-        response = tester.post(
-            '/login',
-            data=dict(username="admin", password="admin"),
-            follow_redirects=True
-        )
-        self.assertIn(b'Te has conectado', response.data)
+        with tester:
+            response = tester.post(
+                '/login',
+                data=dict(username="admin", password="admin"),
+                follow_redirects=True
+            )
+            assert request.path == url_for('portafolio')
+            self.assertIn(b'Te has conectado', response.data)
 
     # Verifica que el login funciona correctamente cuando se dan las credenciales incorrectas
     def test_incorrect_login(self):
         tester = app.test_client()
-        response = tester.post(
-            '/login',
-            data=dict(username="wrong", password="wrong"),
-            follow_redirects=True
-        )
-        self.assertIn(b'Credenciales invalidas', response.data)
+        with tester:
+            response = tester.post(
+                '/login',
+                data=dict(username="wrong", password="wrong"),
+                follow_redirects=True
+            )
+            assert request.path == url_for('login')
+            self.assertIn(b'Credenciales invalidas', response.data)
     
     # Verifica que el logout funciona correctamente
     def test_logout(self):
@@ -80,10 +87,30 @@ class FlaskTestCase(unittest.TestCase):
     # --------------------------------------------------------------------------
 
     # Verifica que el registro funciona correctamente
+    # def test_user_registeration(self):
+    #     with self.client:
+    #         response = self.client.post('register/', data=dict(
+    #             username='Michael', email='michael@realpython.com',
+    #             password='python', confirm='python'
+    #         ), follow_redirects=True)
+    #         self.assertIn(b'Welcome to Flask!', response.data)
+    #         self.assertTrue(current_user.name == "Michael")
+    #         self.assertTrue(current_user.is_active())
+    #         user = User.query.filter_by(email='michael@realpython.com').first()
+    #         self.assertTrue(str(user) == '<name - Michael>')
 
-    # Verifica que se muestra error si se realiza un registro incorrecto
 
-    # Verificar que el registro se guarda en la database
+    # Verifica que se muestra error si se realiza un registro incorrecto (ya sea un user que ya existe, una contraseña mala...)
+    # def test_incorrect_user_registeration(self):
+    #     with self.client:
+    #         response = self.client.post('register/', data=dict(
+    #             username='Michael', email='michael',
+    #             password='python', confirm='python'
+    #         ), follow_redirects=True)
+    #         self.assertIn(b'Invalid email address.', response.data)
+    #         self.assertIn(b'/register/', request.url)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
