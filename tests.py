f14bbb2ef31845f5bb6ab52cf78f5b7a6ad33d42
fungikami <kafung09@gmail.com>
2022-06-20 16:32:37 -0400

    Tests Tipo de Productor

--- tests.py
+++ tests.py
@@ -141,7 +141,7 @@ class PerfilesTestCase(unittest.TestCase):
                 password="admin", rol=1, cosecha=''
             ), follow_redirects=True)
             assert request.path == url_for('perfiles')
-            # self.assertIn(b'El nombre de usuario no puede tener mas de 20 caracteres', response.data)
+            self.assertIn(b'El nombre de usuario no puede tener mas de 20 caracteres', response.data)
 
             # Registrarse a si mismo
             response = tester.post('/perfiles', data=dict(
@@ -149,7 +149,7 @@ class PerfilesTestCase(unittest.TestCase):
                 password="admin", rol=1, cosecha=''
             ), follow_redirects=True)
             assert request.path == url_for('perfiles')
-            # self.assertIn(b'El nombre de usuario ya se encuentra en uso', response.data)
+            self.assertIn(b'El nombre de usuario ya se encuentra en uso', response.data)
             
             # Registrarse con contraseña muy corta
             response = tester.post('/perfiles', data=dict(
@@ -157,7 +157,7 @@ class PerfilesTestCase(unittest.TestCase):
                 password="pr", rol=1, cosecha=''
             ), follow_redirects=True)
             assert request.path == url_for('perfiles')
-            # self.assertIn(b'debe tener al menos 8 caracteres', response.data)
+            self.assertIn(b'debe tener al menos 8 caracteres', response.data)
             
             # Registrarse con contraseña muy larga
             response = tester.post('/perfiles', data=dict(
@@ -166,7 +166,7 @@ class PerfilesTestCase(unittest.TestCase):
                 rol=1, cosecha=''
             ), follow_redirects=True)
             assert request.path == url_for('perfiles')
-            # self.assertIn(b'no puede tener mas de 80 caracteres', response.data)
+            self.assertIn(b'no puede tener mas de 80 caracteres', response.data)
     
     #  Verifica que se puede eliminar un perfil
     def test_correct_delete(self):
@@ -443,14 +443,15 @@ class TipoProductorCase(unittest.TestCase):
                     description='Prueba'
                 ), follow_redirects=True)
 
+            type = TypeProducer.query.filter_by(description='Prueba').first()
+            self.assertTrue(type is not None)           
+
             # Registra tipo de productor de nuevo
-            tester.post('/tipo_productor', data=dict(
+            response = tester.post('/tipo_productor', data=dict(
                     description='Prueba'
                 ), follow_redirects=True)
 
-            response = tester.get('/tipo_productor', follow_redirects=True)
-            self.assertIn(b'Tipos de Productor', response.data)
-            # self.assertIn(b'El tipo de productor ya se encuentra definido.', response.data)
+            self.assertIn(b'El tipo de productor ya se encuentra definido.', response.data)
 
     #  Verifica que se puede eliminar un tipo de productor
     def test_correct_delete(self):
@@ -472,12 +473,9 @@ class TipoProductorCase(unittest.TestCase):
             self.assertTrue(type is not None)
 
             # Elimina tipo de productor
-            tester.post('/delete_tipo_productor/{{ type.id }}', follow_redirects=True)
-
-            response = tester.get('/tipo_productor', follow_redirects=True)
-            self.assertIn(b'Tipos de Productor', response.data)
+            tester.post('/delete_tipo_productor/' + str(type.id), follow_redirects=True)
             type = TypeProducer.query.filter_by(description='Prueba').first()
-            # self.assertTrue(type is None)
+            self.assertTrue(type is None)
 
     #  Verifica que no se puede eliminar un tipo de productor que no existe
     def test_incorrect_delete(self):
@@ -490,11 +488,24 @@ class TipoProductorCase(unittest.TestCase):
                 follow_redirects=True
             )
 
-            # Elimina tipo de productor
-            tester.post('/delete_tipo_productor/100', follow_redirects=True)
+            # Registra tipo de productor
+            tester.post('/tipo_productor', data=dict(
+                    description='Prueba'
+                ), follow_redirects=True)
 
-            response = tester.get('/tipo_productor', follow_redirects=True)
-            self.assertIn(b'Tipos de Productor', response.data)
+            type = TypeProducer.query.filter_by(description='Prueba').first()
+            self.assertTrue(type is not None)
+            id = str(type.id)
+
+            # Elimina tipo de productor
+            tester.post('/delete_tipo_productor/' + id, follow_redirects=True)
+            type = TypeProducer.query.filter_by(description='Prueba').first()
+            self.assertTrue(type is None)
+
+            # Elimina tipo de productor de nuevo
+            tester.post('/delete_tipo_productor/'  + id, follow_redirects=True)
+            type = TypeProducer.query.filter_by(description='Prueba').first()
+            self.assertTrue(type is None)
 
     #  Verifica que se puede editar un tipo de productor
     def test_correct_edit(self):
@@ -516,13 +527,14 @@ class TipoProductorCase(unittest.TestCase):
             self.assertTrue(type is not None)
 
             # Edita tipo de productor
-            tester.post('/update_tipo_productor/{{ type.id }}', data=dict(
+            tester.post('/update_tipo_productor/' + str(type.id), data=dict(
                     description='Prueba2'
                 ), follow_redirects=True)
 
-            response = tester.get('/tipo_productor', follow_redirects=True)
-            self.assertIn(b'Tipos de Productor', response.data)
-            type = TypeProducer.query.filter_by(description='Prueba2').first()
+            # type = TypeProducer.query.filter_by(description='Prueba').first()
+            # self.assertTrue(type is None)
+
+            # type = TypeProducer.query.filter_by(description='Prueba2').first()
             # self.assertTrue(type is not None)
 
     #  Verifica que no se puede editar un tipo de productor que no existe
@@ -536,16 +548,20 @@ class TipoProductorCase(unittest.TestCase):
                 follow_redirects=True
             )
 
-            # Edita tipo de productor
-            tester.post('/update_tipo_productor/100', data=dict(
-                    description='Prueba2'
+            # Registra tipo de productor
+            tester.post('/tipo_productor', data=dict(
+                    description='Prueba'
                 ), follow_redirects=True)
 
+            type = TypeProducer.query.filter_by(description='Prueba').first()
+            self.assertTrue(type is not None)
 
-            response = tester.get('/tipo_productor', follow_redirects=True)
-            self.assertIn(b'Tipos de Productor', response.data)
-            type = TypeProducer.query.filter_by(description='Prueba2').first()
-            self.assertTrue(type is None)
+            # Edita tipo de productor a uno que ya se encuentra en uso
+            response = tester.post('/update_tipo_productor/' + str(type.id), data=dict(
+                    description='Prueba'
+                ), follow_redirects=True)
+
+            self.assertIn(b'El tipo de productor ya se encuentra definido.', response.data)
 
 
     #  Verifica que se puede buscar un tipo de productor
@@ -571,8 +587,6 @@ class TipoProductorCase(unittest.TestCase):
             response = tester.get('/tipo_productor', follow_redirects=True)
             self.assertIn(b'Tipos de Productor', response.data)
             self.assertIn(b'Prueba', response.data)
-
-
 
 if __name__ == '__main__':
     unittest.main()
